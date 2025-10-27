26e89db256003a03a197910345e38aa8d40aeac1
xfgt <waffle_man562@yahoo.com>
2025-10-27 13:00:55 +0200

    exception and list of perimeters

--- src/main/java/Triangle/service/TriangleService.java
+++ src/main/java/Triangle/service/TriangleService.java
@@ -2,6 +2,11 @@ package Triangle.service;
 
 import Triangle.data.Triangle;
 
+import java.util.ArrayList;
+
 public interface TriangleService {
-    double perimeter(Triangle triangle);
+    double perimeter(Triangle triangle) throws IllegalArgumentException;
+
+
+    ArrayList<Double> listOfPerimeters(ArrayList<Triangle> triangles);
 }
