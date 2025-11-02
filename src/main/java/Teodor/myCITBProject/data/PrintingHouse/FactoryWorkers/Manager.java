13dd27760744134ff9c11c232fd3c79d6926ad6b
xfgt <waffle_man562@yahoo.com>
2025-11-02 09:26:48 +0200

    Workers fix

--- src/main/java/Teodor/myCITBProject/data/PrintingHouse/FactoryWorkers/Manager.java
+++ src/main/java/Teodor/myCITBProject/data/PrintingHouse/FactoryWorkers/Manager.java
@@ -1,18 +1,26 @@
 package Teodor.myCITBProject.data.PrintingHouse.FactoryWorkers;
 
 public class Manager extends Employee{
+
+
+    /*
+        определен процент върху основното заплащане, ако приходите на печатницата са повече от
+        определена стойност, която се задава в печатницата.
+     */
     private double percentBonus;
-    private double profitCeiling;
+    private double profitCeiling; // определената стойност
 
     public Manager(double baseSalary, double percentBonus, double profitCeiling) {
         super(baseSalary);
         this.percentBonus = percentBonus;
         this.profitCeiling = profitCeiling;
+
+        // TODO: negative values?
     }
 
     @Override
-    public double calculateSalary(double profit) {
-        if(profit > profitCeiling)
+    public double calculateSalary(double printingHouseProfits) {
+        if(printingHouseProfits > profitCeiling)
             return getBaseSalary() + (getBaseSalary() * percentBonus / 100);
         return getBaseSalary();
     }
